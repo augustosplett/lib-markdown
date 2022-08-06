cd2e95bae91d3aa5bb273059d2e59cad325d0ef8
Augusto Madeira Splett (Tinho) <augusto.splett@gmail.com>
2022-08-06 17:36:25 -0300

    implementada função Regex para retornar nomes e links do texto

--- index.js
+++ index.js
@@ -3,6 +3,16 @@ import fs from 'fs';
 
 const arqPath = './arquivos/texto1.md';
 
+function extraiLinks(texto) {
+    const regex = /\[([^\]]*)\]\((https?:\/\/[^$#\s].[^\s]*)\)/gm;
+    const arrayResultados = [];
+    let temp;
+    while((temp = regex.exec(texto)) !== null) {
+      arrayResultados.push({ [temp[1]]: temp[2] })
+    }
+    return arrayResultados;
+}
+
 function handleError(error){
     throw new Error(chalk.red(error.code, 'File not found'));
 }
@@ -11,7 +21,7 @@ function loadFile(path){
     const encoding = 'utf-8'
     fs.promises
     .readFile(path, encoding)
-    .then( text => console.log(chalk.blue(text)))
+    .then( text => console.log(extraiLinks(text)))
     .catch( error => handleError(error))
 }
 
