6371e60520ba4fc0b7ee8b8187310f707fd926fe
Augusto Madeira Splett (Tinho) <augusto.splett@gmail.com>
2022-08-10 23:05:51 -0300

    implementadas funcionalidades para validar se os links estão funcionais

--- cli.js
+++ cli.js
@@ -6,7 +6,7 @@ const caminho = process.argv;
 async function processaArquivo(filePath){
     let result = await loadFile(filePath[2]);
     if(filePath[3] === 'validar') {
-        console.log(chalk.yellow('Linkis Validados: '), validaUrls(result));
+        console.log(chalk.yellow('Linkis Validados: '), await validaUrls(result));
     }else{
         console.log(chalk.yellow('Lista de links:'), result)
     }
--- http-validacao.js
+++ http-validacao.js
@@ -1,3 +1,35 @@
-export default function validaUrls(arrayLinks){
-    return arrayLinks;
+import fetch from "node-fetch";
+
+function handleError(err){
+    throw new Error(err.message);
 }
+
+async function checaStatus(arrayLinks){
+    try{
+        const arrayStatus = await Promise
+            .all(arrayLinks
+                .map(async url => {
+                    const res = await fetch(url)
+                    return res.status;
+        }))
+        return arrayStatus;
+    }catch(err){
+        handleError(err);
+    }
+}
+
+function geraArrayUrls(arrayUrls){
+    return arrayUrls
+        .map(element => Object.values(element).join())
+}
+
+export default async function validaUrls(arrayLinks){
+    const links = geraArrayUrls(arrayLinks);
+    const statusLinks = await checaStatus(links);
+    const resultados = arrayLinks
+        .map((obj, index) => ({
+            ...obj, status: statusLinks[index]
+        }))
+    return resultados;
+}
+
